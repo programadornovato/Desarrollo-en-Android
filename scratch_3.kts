c6e920ad479ab25d064e32dd4566a32d0a7552ee
Programador Novato <info@programadornovato.com>
2020-12-13 02:12:04 -0600

    Operadores de asignación en  Android Studio [21]
    Video:
    https://youtu.be/9UlGVJphfYs

---
+++ scratch_3.kts
@@ -0,0 +1,22 @@
+var a=6.0
+var b=5.0
+println("a=$a y b=$b")
+/*
+//a=a+b
+a+=b
+println("a=$a y b=$b")
+//a=a-b
+a-=b
+println("a=$a y b=$b")
+//a=a*b
+a*=b
+println("a=$a y b=$b")
+//a=a/b
+a/=b
+println("a=$a y b=$b")
+*/
+//a=a%b
+a%=b
+println("a=$a y b=$b")
+
+

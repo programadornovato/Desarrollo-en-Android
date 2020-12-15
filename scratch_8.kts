3378f896d362f8a2cf10b6af9f40a6e6dd077640
Programador Novato <info@programadornovato.com>
2020-12-14 19:47:07 -0600

    Condicionales con String en Android Studio [26]
    Video:
    https://youtu.be/HrUbu4bcFT4

---
+++ scratch_8.kts
@@ -0,0 +1,25 @@
+/*
+println("Hola humano, por favor inres alguno de los textos de este menu \nacceso\nconfiguracion\nayuda")
+val seleccion= readLine()!!.toLowerCase()
+when(seleccion){
+    "acceso"-> println("Seleccionaste acceso")
+    "configuracion"-> println("Seleccionaste configuracion")
+    "ayuda"-> println("Seleccionaste ayuda")
+    else -> println("Humano tonto no seleccionaste nuguna de las opciones")
+}
+
+println("Humano por favor saludame con un hola")
+val saludo= readLine()
+if(saludo=="hola"){
+    println("Hola humano como estas")
+}else{
+    println("No entendi")
+}
+*/
+println("Humano por favor saludame con un hola")
+val saludo= readLine()
+if(saludo.equals("hola")==true){
+    println("Hola humano como estas")
+}else{
+    println("No entendi")
+}

2638cb3aa66953f46a12ec1201f0ce26ef9f144b
Programador Novato <info@programadornovato.com>
2020-12-14 19:11:36 -0600

    Ingresar datos de forma gráfica con showInputDialog en Android Studio [25]
    Video:
    https://youtu.be/wbiQELwWHaw

---
+++ scratch_7.kts
@@ -0,0 +1,16 @@
+import javax.swing.JOptionPane
+/*
+val cadena=JOptionPane.showInputDialog("Mete el texto")
+JOptionPane.showMessageDialog(null,cadena)
+
+var numero=JOptionPane.showInputDialog("Mete el numero").toInt()
+numero++
+JOptionPane.showMessageDialog(null,numero)
+
+var doble=JOptionPane.showInputDialog("Mete el numero con punto decimal").toDouble()
+doble++
+JOptionPane.showMessageDialog(null,doble)
+*/
+
+val caracter=JOptionPane.showInputDialog("Inresa un texto")[3]
+JOptionPane.showMessageDialog(null,caracter)

c0f050d4d010732bb0bce770a450704bb14b5af0
Programador Novato <info@programadornovato.com>
2020-12-16 01:50:39 -0600

    Ciclo do while en Android Studio [30]
    Video:
    https://youtu.be/-FVP64hPL5A

---
+++ scratch_13.kts
@@ -0,0 +1,21 @@
+/*
+var i=11
+do{
+    println("i=$i")
+    i++
+}while (i<=10)
+
+var i=10
+do{
+    println("i=$i")
+    i--
+}while (i>=1)
+*/
+val aleatorio=(1..6).random()
+println("aleatorio=$aleatorio")
+var i=3
+do{
+    println("i=$i")
+    i++
+}while (i<=aleatorio)
+
